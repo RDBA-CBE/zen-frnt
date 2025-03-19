23c6aee72b636b4afe1a0ac1499d52ff829d5052
Alaguraj1 <alaguraj259@gmail.com>
2025-03-19 14:19:36 +0530

    changes

--- components/common-components/ChangePasswordConfirm.jsx
+++ components/common-components/ChangePasswordConfirm.jsx
@@ -120,7 +120,7 @@ const ChangePasswordConfirmForm = () => {
         <div className="flex items-center justify-center">
             <Card className="md:w-[400px] w-[100%]">
                 <CardHeader>
-                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardTitle className="text-2xl">Confirm Password</CardTitle>
                     {/* <CardDescription>
                         Enter your email below to login to your account
                     </CardDescription> */}
