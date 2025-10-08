f0b974eedc6e2e243d66316e0cf94c414a68dbe7
nsathiyakala <nsathiyakala6@gmail.com>
2025-10-08 14:09:14 +0530

    header

--- components/common-components/header.jsx
+++ components/common-components/header.jsx
@@ -396,7 +396,7 @@ const Header = () => {
                     </Avatar>
                   </DropdownMenuTrigger>
                   <DropdownMenuContent
-                    className="bg-fuchsia-100 w-[220px] p-4 rounded-lg"
+                    className="bg-fuchsia-100 w-[280px] p-4 rounded-lg"
                     side="bottom"
                     align="end"
                     sideOffset={4}
